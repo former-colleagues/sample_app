c96796caf387b118278f63d66030943009f309f0
ryo-kondo <wonton0118@gmail.com>
2018-02-24 17:58:46 +0900

    Revert "rails tutorial sample_app"
    This reverts commit 4aa0b5d062998e5f4093bf30638c8b99aa4bc3e7.

--- sample_app/db/migrate/20180216004531_add_in_reply_to_to_microposts.rb
+++
@@ -1,5 +0,0 @@
-class AddInReplyToToMicroposts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :microposts, :in_reply_to, :string, default: ""
-  end
-end
--- sample_app/db/migrate/20180219014134_add_notification_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNotificationToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :notification, :boolean, default: true
-  end
-end
