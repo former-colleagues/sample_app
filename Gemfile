5cd670adf3f319b5154bd173db0ad80e9c549c11
godai0505 <tanagoda.tech@gmail.com>
2018-03-12 15:16:06 +0000

    rails_adminの導入

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks', '5.0.1'
 gem 'jbuilder', '2.7.0'
 gem 'therubyracer', platforms: :ruby
+gem 'rails_admin'
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
